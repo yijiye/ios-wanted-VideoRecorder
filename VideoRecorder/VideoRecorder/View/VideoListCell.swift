dc28f4284931e93a8c477559b124d7d572004d77
yijiye <jiyee4013@naver.com>
2023-06-05 17:13:54 +0900

    feat: DateFormat 기능 구현

--- VideoRecorder/VideoRecorder/View/VideoListCell.swift
+++ VideoRecorder/VideoRecorder/View/VideoListCell.swift
@@ -10,10 +10,19 @@ import UIKit
 final class VideoListCell: UICollectionViewListCell {
     var video: Video?
     
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        return dateFormatter
+    }()
+    
     override func updateConfiguration(using state: UICellConfigurationState) {
         var configuration = VideoContentConfiguration().updated(for: state)
+        guard let date = video?.date else { return }
+        let convertedDate = dateFormatter.string(from: date)
         configuration.title = video?.title
-        configuration.date = video?.date.description
+        configuration.date = convertedDate
         configuration.videoImage = video?.image
         
         contentConfiguration = configuration
