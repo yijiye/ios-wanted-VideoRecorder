b2d00141694eafe6597996929781493d07a4b81c
yijiye <jiyee4013@naver.com>
2023-06-05 14:09:27 +0900

    feat: NavigationBar 구현

--- VideoRecorder/VideoRecorder/SceneDelegate.swift
+++ VideoRecorder/VideoRecorder/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: VideoListViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
